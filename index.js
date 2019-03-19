6c2fa941c3ab550383fb37034de6dd4e07b14ffd
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-03-18 19:59:53 -0700

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+$(function () {
+    const quizTemplateScript = $("#quiz-template").html();
+
+    // Compile the template
+    const quizTemplate = Handlebars.compile(quizTemplateScript);
+
+    // Define data object
+    let quiz = {
+        
+    }
+})
