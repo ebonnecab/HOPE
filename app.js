20051205bfe7cd86405be6c4505911055eb1048d
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-03-18 20:50:54 -0700

    rendering home

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+// Dependencies
 const express = require('express')
 const app = express()
+const exphbs = require("express-handlebars");
+
+//Templating engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render("home", { msg: "Handlebars are Cool!" });
 })
 
 app.listen(3000, () => {
