ae85c4142a2bad8ea03376f1b3adf67adddbaba5
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-03-19 13:09:07 -0700

    quiz gets name from user and shows questions

--- quiz/quiz_interaction.js
+++ quiz/quiz_interaction.js
@@ -26,5 +26,55 @@ let hopeQuiz = {
     } else {
         // what to do if local storage doesn't exist
     }
+  },
+
+  welcome: function(name) {
+      hopeQuiz.clearQuiz();
+      let greeting = document.createElement('h2')
+      let greetingText = document.createTextNode('Hello ' + name + '!');
+      greeting.appendChild(greetingText);
+      hopeQuiz.element.appendChild(greeting);
+      let buttonText = "Not " + name + "?";
+      hopeQuiz.nameChangeButton.textContent = buttonText;
+      hopeQuiz.nameChangeButton.addEventListener('click', hopeQuiz.askForName);
+      hopeQuiz.startButton.addEventListener('click', hopeQuiz.next);
+      hopeQuiz.nextButton.addEventListener('click', hopeQuiz.next);
+  },
+
+  askForName: function() {
+      hopeQuiz.clearQuiz();
+      let form = document.getElementById('nameForm');
+      form.addEventListener('submit', function(e){
+          e.preventDefault(); //prevents form submission
+          let name = document.getElementById('username').value;
+          localStorage.setItem('name', name);
+          form.style.display = "none";
+          hopeQuiz.checkName(name);
+      });
+  },
+
+  showQuestion: function(bookmark) {
+      hopeQuiz.element.innerHTML = "<h1>" + hopeQuiz.allQuestions[hopeQuiz.index]["question"] + "</h1>";
+
+      //creates choices as radio input
+      for (let i = 0; i < hopeQuiz.allQuestions[hopeQuiz.index]["choices"].length; i++) {
+          let choice = document.createElement('input');
+          choice.type = 'radio';
+          choice.className = 'choices';
+          choice.value = i;
+          
+          // if user chose current choice as answer, mark current choice
+          if ('answer' in hopeQuiz.allQuestions[hopeQuiz.index] && choice.value == hopeQuiz.allQuestions[hopeQuiz.index]['answer']) {
+              choice.checked = "checked";
+          }
+
+          //append current choice to quiz
+          hopeQuiz.element.appendChild(choice);
+          hopeQuiz.element.appendChild(document.createTextNode(hopeQuiz.allQuestions[hopeQuiz.index]['choices'][i]));
+          hopeQuiz.element.appendChild(document.createElement('br'));
+    
+      }
+
+      console.log(quiz.index);
   }
 };
