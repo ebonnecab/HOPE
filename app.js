804a8bd2bd9b69adf60dd5c4ea5ae0b53eae9c65
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-03-18 21:20:24 -0700

    added mongoose and Quiz model

--- app.js
+++ app.js
@@ -7,9 +7,34 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// mongodb
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/HOPE", {
+  useNewUrlParser: true
+});
+
+// model
+const Quiz = mongoose.model('Quiz', {
+    Question: String,
+    Answer: String
+});
+
 app.get('/', (req, res) => {
     res.render("home", { msg: "Handlebars are Cool!" });
 })
+
+//Index
+app.get('/quiz', (req, res) => {
+    Quiz.find()
+        .then(quiz => {
+            res.render('quiz-index', { quiz: quiz });
+        })
+        .catch(err => {
+            console.log(err);
+        })
+})
+
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
