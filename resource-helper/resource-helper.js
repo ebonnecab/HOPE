6a4859e4cc8fc681693ab937bcc73c0ff0b557b1
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-03-21 12:18:10 -0700

    psuedo coded logic for resource helper

---
+++ resource-helper/resource-helper.js
@@ -0,0 +1,5 @@
+// PSUEDO CODE: If the user chooses "this answer", the helper
+// display the programs from the corresponding json object in the browser
+// it needs to be able to access the answers from the quiz, store them,
+// access the different json files for each question, and match the answer
+// with the correct "type" in order to display the right programs
